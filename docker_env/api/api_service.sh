50ffda50ea080aabc5c8cea94ac0fff397bda39c
Alexandr Shevchenko <ls.shov@gmail.com>
2020-04-07 00:44:45 +0300

    Fixed API service startiing script

--- docker_env/api/api_service.sh
+++ docker_env/api/api_service.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [ ! -d /app/build ]; then
-  echo "Skip app starting, not built yet."
+echo "Starting the app"
+
+#Dot env
+if [[ ! -f /app/.env ]]; then
+  echo ".env file not found! Starting failed!"
+  exit 1
 else
-  echo "Starting the app"
-
-  #Dot env
-  if [[ ! -f /app/.env ]]; then
-    echo ".env file not found! Starting failed!"
-    exit 1
-  else
-    set -a
-    . "/app/.env"
-    set +a
-  fi
-
-  cd /app &&
-    ./gradlew clean build &&
-    ./gradlew bootRun -Pargs=--db.url=jdbc:postgresql://${DATA_DB_HOST}:${DATA_DB_PORT}/${DATA_DB_NAME},--db.username=${DATA_DB_USER},--db.password=${DATA_DB_PASSWORD},--db.max-pool-size=10,--auth.client-token=${DEFAULT_CLIENT_ID},--port=${APP_SERVICE_PORT}
+  set -a
+  . "/app/.env"
+  set +a
 fi
+
+cd /app &&
+  ./gradlew clean build &&
+  ./gradlew bootRun -Pargs=--db.url=jdbc:postgresql://${DATA_DB_HOST}:${DATA_DB_PORT}/${DATA_DB_NAME},--db.username=${DATA_DB_USER},--db.password=${DATA_DB_PASSWORD},--db.max-pool-size=10,--auth.client-token=${DEFAULT_CLIENT_ID},--port=${APP_SERVICE_PORT}
