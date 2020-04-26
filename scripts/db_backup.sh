a8359ade214f63f55540b6d2d7608c12a18cd2f8
Alexandr Shevchenko <ls.shov@gmail.com>
2020-04-27 01:50:51 +0300

    db-backups: finished, not tested

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -12,3 +12,42 @@ else
   set +a
 fi;
 
+NOW_DATE_TAG=`date +%Y-%m-%d-%H-%M-%S`
+DUMP_CMD=$(docker-compose exec -T postgres which pg_dump)
+
+FILE_NAME=${DB_NAME}.${NOW_DATE_TAG}.sql
+BAK_PATH="${SCRIPT_PATH}/../backups/db"
+
+mkdir -p ${BAK_PATH}
+
+echo "+ Start dumping"
+
+DUMPED=0
+docker-compose exec -T postgres bash -c "PGPASSWORD=${DATA_DB_PASSWORD} ${DUMP_CMD} -U ${DATA_DB_USER} -F p -n qarantinno ${DATA_DB_NAME} > /${FILE_NAME}" \
+    && DUMPED=1
+
+if [[ ${DUMPED} -gt 0 ]]; then
+    echo "+ Dumped in /${FILE_NAME} in postgres container"
+else
+    echo "x Can't dump"
+    exit 1
+fi;
+
+COPIED_OUT=0
+docker cp "$(docker-compose ps -q postgres)":/${FILE_NAME} "${BAK_PATH}/${FILE_NAME}" \
+    && COPIED_OUT=1
+
+if [[ ${COPIED_OUT} > 0 ]]; then
+    echo "+ Extracted ${BAK_PATH}/${FILE_NAME}"
+else
+    echo "x Can't extract from the container"
+    exit 1
+fi;
+
+echo "Clean up..."
+
+#TODO: add checking of success
+docker-compose exec -T postgres rm /${FILE_NAME}
+find ${BAK_PATH} -type f -name \*${DB_NAME}\*.sql -mtime +30 -exec rm -f {} \;
+
+echo "+ Done"
