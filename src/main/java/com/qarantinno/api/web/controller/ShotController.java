f27c5410c4a82943e1d3ea0cb450a86867a861b0
brutskov <rutkovba@gmail.com>
2020-05-07 22:53:53 +0300

    Client user token support was added

--- src/main/java/com/qarantinno/api/web/controller/ShotController.java
+++ src/main/java/com/qarantinno/api/web/controller/ShotController.java
@@ -1,19 +1,24 @@
 package com.qarantinno.api.web.controller;
 
 import com.qarantinno.api.domain.Shot;
+import com.qarantinno.api.domain.execption.IllegalOperationException;
 import com.qarantinno.api.service.PlaceService;
 import com.qarantinno.api.web.dto.ShotDTO;
 import com.qarantinno.api.web.dto.ValidationGroups;
 import io.swagger.annotations.Api;
 import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.qarantinno.api.domain.execption.IllegalOperationException.IllegalOperationExceptionType.ILLEGAL_SHOT_CREATE;
 
 @Api("Shot controller documentation")
 @ResponseStatus(HttpStatus.OK)
@@ -22,16 +27,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShotController {
 
     private final PlaceService placeService;
+    private final String clientUserToken;
     private final Mapper mapper;
 
-    public ShotController(PlaceService placeService, Mapper mapper) {
+    public ShotController(
+            PlaceService placeService,
+            @Value("${app.auth.client-user-token}") String clientUserToken,
+            Mapper mapper
+    ) {
         this.placeService = placeService;
+        this.clientUserToken = clientUserToken;
         this.mapper = mapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ShotDTO create(@RequestBody @Validated(ValidationGroups.ExternalOnCreate.class) ShotDTO shotDTO) {
+    public ShotDTO create(@RequestBody @Validated(ValidationGroups.ExternalOnCreate.class) ShotDTO shotDTO, @RequestHeader("client-user-token") String clientUserToken) {
+        if (!this.clientUserToken.equals(clientUserToken)) {
+            throw new IllegalOperationException(ILLEGAL_SHOT_CREATE, "Cannot create shot: illegal access");
+        }
         Shot shot = mapper.map(shotDTO, Shot.class);
         shot = placeService.addShot(shot);
         return mapper.map(shot, ShotDTO.class);
--- src/main/java/com/qarantinno/api/web/controller/StatisticsController.java
+++ src/main/java/com/qarantinno/api/web/controller/StatisticsController.java
@@ -2,17 +2,22 @@ package com.qarantinno.api.web.controller;
 
 import com.qarantinno.api.domain.Statistics;
 import com.qarantinno.api.domain.criteria.StatisticsCriteria;
+import com.qarantinno.api.domain.execption.IllegalOperationException;
 import com.qarantinno.api.service.StatisticsService;
 import com.qarantinno.api.web.dto.criteria.StatisticsCriteriaDTO;
 import com.qarantinno.api.web.dto.StatisticsDTO;
 import io.swagger.annotations.Api;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.qarantinno.api.domain.execption.IllegalOperationException.IllegalOperationExceptionType.ILLEGAL_SHOT_CREATE;
 
 @Api("Statistics controller documentation")
 @ResponseStatus(HttpStatus.OK)
@@ -21,15 +26,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
+    private final String clientUserToken;
     private final Mapper mapper;
 
-    public StatisticsController(@Qualifier("statisticsServiceV2Impl") StatisticsService statisticsService, Mapper mapper) {
+    public StatisticsController(
+            @Qualifier("statisticsServiceV2Impl") StatisticsService statisticsService,
+            @Value("${app.auth.client-user-token}") String clientUserToken,
+            Mapper mapper
+    ) {
         this.statisticsService = statisticsService;
+        this.clientUserToken = clientUserToken;
         this.mapper = mapper;
     }
 
     @GetMapping
-    public StatisticsDTO getAll(StatisticsCriteriaDTO criteriaDTO) {
+    public StatisticsDTO getAll(StatisticsCriteriaDTO criteriaDTO, @RequestHeader("client-user-token") String clientUserToken) {
+        if (!this.clientUserToken.equals(clientUserToken)) {
+            throw new IllegalOperationException(ILLEGAL_SHOT_CREATE, "Cannot create shot: illegal access");
+        }
         StatisticsCriteria criteria = mapper.map(criteriaDTO, StatisticsCriteria.class);
         Statistics statistics = statisticsService.calculateStatistics(criteria);
         return mapper.map(statistics, StatisticsDTO.class);
